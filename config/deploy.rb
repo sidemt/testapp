0e5cd6caeecf1ccbbd60174011f85104c761a7ee
sidemt <siideemt@gmail.com>
2021-09-22 23:43:22 +0900

    Configure Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,16 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "testapp"
+set :repo_url, "git@github.com:sidemt/testapp.git"
+
+# Deploy to the user's home directory
+set :deploy_to, "/home/deploy/#{fetch :application}"
+
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+
+# Only keep the last 5 releases to save disk space
+set :keep_releases, 5
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
